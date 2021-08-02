5dffefa95d2f214f438e00a5645069373513a53c
Leon Tenório <leon_lts@hotmail.com>
2021-08-01 21:21:43 -0300

    refactor: improve test description

--- tdd_stock/src/money.spec.ts
+++ tdd_stock/src/money.spec.ts
@@ -2,26 +2,23 @@ import { Dollar } from './dolar';
 import { Euro } from './euro';
 
 describe('Money', () => {
-  it('should return 10 when five dollars are multiplied by two and 15 when five dollars are multiplied by three', () => {
+  it('should correctly hanlde dollar multiplication', () => {
     const five: Dollar = new Dollar(5);
     expect(five.times(2).equals(new Dollar(10))).toBe(true);
 
     expect(five.times(3).equals(new Dollar(15))).toBe(true);
   });
 
-  it('should return 10 when five euros are multiplied by two and 15 when five euros are multiplied by three', () => {
+  it('should correctly hanlde euro multiplication', () => {
     const five: Euro = new Euro(5);
     expect(five.times(2).equals(new Euro(10))).toBe(true);
 
     expect(five.times(3).equals(new Euro(15))).toBe(true);
   });
 
-  it('should return true when dollares of the same value are compared', () => {
+  it('should correctly handle money equality', () => {
     expect(new Dollar(5).equals(new Dollar(5))).toBe(true);
     expect(new Dollar(5).equals(new Dollar(6))).toBe(false);
-  });
-
-  it('should return true when euros of the same value are compared', () => {
     expect(new Euro(5).equals(new Euro(5))).toBe(true);
     expect(new Euro(5).equals(new Euro(6))).toBe(false);
   });
