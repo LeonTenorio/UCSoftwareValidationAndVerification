a8123c351e5e3b1886219e2144021d6b2d5068b6
Leon Tenório <leon_lts@hotmail.com>
2021-08-01 21:03:38 -0300

    refactor: make equals a money operation

--- tdd_stock/src/dolar.ts
+++ tdd_stock/src/dolar.ts
@@ -1,15 +1,11 @@
-export class Dollar {
-  private readonly amount: number;
+import { Money } from './money';
 
+export class Dollar extends Money {
   constructor(amount: number) {
-    this.amount = amount;
+    super(amount);
   }
 
   times(multiplier: number): Dollar {
     return new Dollar(this.amount * multiplier);
-  }
-
-  equals(dollar: Dollar): boolean {
-    return this.amount === dollar.amount;
   }
 }
