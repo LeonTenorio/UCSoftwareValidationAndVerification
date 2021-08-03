1d24bdbf784356e6ca1a3bf344413b74c59c6587
Leon Tenório <leon_lts@hotmail.com>
2021-08-03 20:53:03 -0300

    feat: create factory methods for dollars and euros

--- tdd_stock/src/money.ts
+++ tdd_stock/src/money.ts
@@ -1,8 +1,16 @@
-export class Money {
+export abstract class Money {
   protected readonly amount: number;
 
   constructor(amount: number) {
     this.amount = amount;
+  }
+
+  static dollar(amount: number): Dollar {
+    return new Dollar(amount);
+  }
+
+  static euro(amount: number): Euro {
+    return new Euro(amount);
   }
 
   equals(other: Money): boolean {
@@ -10,5 +18,27 @@ export class Money {
       return false;
     }
     return this.amount === other.amount;
+  }
+
+  abstract times(multiplier: number): Money;
+}
+
+export class Dollar extends Money {
+  constructor(amount: number) {
+    super(amount);
+  }
+
+  times(multiplier: number): Dollar {
+    return new Dollar(this.amount * multiplier);
+  }
+}
+
+export class Euro extends Money {
+  constructor(amount: number) {
+    super(amount);
+  }
+
+  times(multiplier: number): Euro {
+    return new Euro(this.amount * multiplier);
   }
 }
