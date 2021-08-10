9282b148247076319af208f237752415ab758881
Leon Tenório <leon_lts@hotmail.com>
2021-08-10 20:34:09 -0300

    feat: improve test descriptions

--- tdd_stock/src/money.spec.ts
+++ tdd_stock/src/money.spec.ts
@@ -1,20 +1,20 @@
 import { Money } from './money';
 
 describe('Money', () => {
-  it('should correctly hanlde dollar multiplication', () => {
+  it('should hanlde dollar multiplication', () => {
     const five: Money = Money.dollar(5);
     expect(five.times(2).equals(Money.dollar(10))).toBe(true);
 
     expect(five.times(3).equals(Money.dollar(15))).toBe(true);
   });
 
-  it('should correctly handle money equality', () => {
+  it('should handle money equality', () => {
     expect(Money.dollar(5).equals(Money.dollar(5))).toBe(true);
     expect(Money.dollar(5).equals(Money.dollar(6))).toBe(false);
     expect(Money.euro(5).equals(Money.dollar(5))).toBe(false);
   });
 
-  it('should correctly handle currencies', () => {
+  it('should handle currencies', () => {
     expect(Money.dollar(1).currency()).toEqual('USD');
     expect(Money.euro(1).currency()).toEqual('EUR');
   });
